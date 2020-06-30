dbf17dfbb9f68067bc6acd468508c29c51a3b854
MIGUELez11 <miguel.mangel.ez11@gmail.com>
2020-06-30 15:19:29 +0200

    H

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = express();
 const port = process.env.PORT || 80
 
 server.get("/", (req, res) => {
-    res.send("Hola Martin" + JSON.stringify(process.env));
+    res.send("Hola", process.env.name);
 })
 
 server.listen(port);
