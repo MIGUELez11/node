565da80d9be1e6b4f431e6a721a8b177ec4db9c8
MIGUELez11 <miguel.mangel.ez11@gmail.com>
2020-07-01 09:56:40 +0200

    Puppeteer

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const p = require("puppeteer");
 const server = express();
 
 const port = process.env.PORT || 80
 
-server.get("/", (req, res) => {
-    res.send("Hola" + process.env.NAME);
+server.get("/", async (req, res) => {
+    try {
+        const browser = await p.launch(
+            {
+                headless: true,
+                //args: ['--no-sandbox']
+            }
+        );
+        const page = await browser.newPage();
+        await page.goto("https://google.com");
+        page.content().then(async(data) => {
+            await page.screenshot({path: "sc.png"});
+            res.send(data);
+            await browser.close()
+        }).catch(e => console.log(e))
+    } catch(e) {
+        res.send(e);
+    }
+
+    //res.send("Hola" + process.env.NAME);
 })
 
 server.listen(port);
